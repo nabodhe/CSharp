aad843a455d5913bc4fc8fb2f087c416a15c020c
intenseNibo <nabodhe@yahoo.com>
2020-08-05 21:38:34 +0530

    Initial Check in Design Patterns & Singleton pattern project.
    Initial Check in Design Patterns & Singleton pattern project.

---
+++ SingletonImpl/MySingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonImpl
+{
+    public sealed class MySingleton
+    {
+        static readonly Lazy<MySingleton> MySingletonObj = new Lazy<MySingleton>(() => new MySingleton());
+
+        public static MySingleton Instance
+        {
+            get
+            {
+                return MySingletonObj.Value;
+            }
+        }
+
+        public string Name { get; private set; }
+        public int Number { get; private set; }
+        private MySingleton()
+        {
+            Number = new Random(50).Next(1, 50);
+            Name = " One ";
+        }
+    }
+}
---
+++ SingletonImpl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SingletonImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Singleton Pattern!");
+            MySingleton s1 = MySingleton.Instance;
+            Console.WriteLine($"s1 = {s1.Name} Num = {s1.Number}");
+            MySingleton s2 = MySingleton.Instance;
+            Console.WriteLine($"s2 = {s2.Name} Num = {s2.Number}");
+            MySingleton s3 = MySingleton.Instance;
+            Console.WriteLine($"s3 = {s3.Name} Num = {s3.Number}");
+            if(s1 == s2 && s1 == s3)
+            {
+                Console.WriteLine($"All references pointing to same object!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
