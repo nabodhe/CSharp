bce61545d92c7f77ecf4ac273b32c7c55a20f712
intenseNibo <nabodhe@yahoo.com>
2020-08-08 23:09:28 +0530

    Task Parallel Lib -> Cancellation Token Demo

---
+++ CSharp_Concepts/TPL_CancellationToken/WinForms/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(65, 32);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 62);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_ClickAsync);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(310, 32);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(200, 62);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox1.Location = new System.Drawing.Point(5, 5);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1009, 153);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Hello World";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.textBox2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 271);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1028, 421);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox2.Location = new System.Drawing.Point(5, 168);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1009, 247);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 692);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Async Await & Cancel";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CSharp_Concepts/TPL_CancellationToken/WinForms/HelloWorld/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnStart_ClickAsync(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            CallReadFunction();
+        }
+        ReadAndDisplayFile readObj;
+        private async void CallReadFunction()
+        {
+            readObj = new ReadAndDisplayFile();
+            var count = await readObj.ReadFileOrCancelOperation();
+            textBox1.Text = $" Lines read {count.Item1}";
+            textBox2.Text = $" Lines \n { count.Item2}";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            readObj?.Cancel();
+        }
+
+        class ReadAndDisplayFile
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+            public async Task<Tuple<int,string>> ReadFileOrCancelOperation()
+            {
+                int count = 0;
+                StringBuilder message = new StringBuilder();
+                message.AppendLine("Begining reading File");
+                using (FileStream fs = new FileStream(@"Repo\HelloWorldRepo.txt", FileMode.Open))
+                {
+                    StreamReader sr = new StreamReader(fs);
+                    while (!sr.EndOfStream)
+                    {
+                        if (source.IsCancellationRequested)
+                        {
+                            message.AppendLine($"Task Cancelled. Count of words {count}");
+                            break;
+                        }
+                        string strLine = sr.ReadLine();
+                        count += strLine.Length;
+                        await Task.Delay(200);
+                        message.AppendLine($"Count of words {count}");
+                        if (source.IsCancellationRequested)
+                        {
+                            message.AppendLine($" Task Cancelled. Count of words {count}");
+                            break;
+                        }
+                    }
+                    sr.Close();
+                    sr.Dispose();
+                }
+                if (!source.IsCancellationRequested) 
+                    message.AppendLine($" Task Finished. Count of words {count}");
+                return new Tuple<int,string>(count, message.ToString());
+            }
+
+            public bool Cancel()
+            {
+                bool result = false;
+                if (source != null)
+                {
+                    source.Cancel();
+                    result = true;
+                }
+                return result;
+            }
+        }
+    }
+}
