7fa3849bb077fab9657e47d7ff0db49925cffa71
intenseNibo <nabodhe@yahoo.com>
2020-08-08 19:05:09 +0530

    Check in Abstract factory changes

---
+++ AbstractFactoryImpl/Program.cs
@@ -0,0 +1,52 @@
+﻿using AbstractFactoryImpl.Common;
+using AbstractFactoryImpl.Interfaces;
+using System;
+using static System.Console;
+
+namespace AbstractFactoryImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IVehicleFactory factory = null;
+            CreateAndDisplayCar(ref factory);
+            CreateAndDisplayTruck(ref factory);
+            ReadKey();
+
+        }
+
+        private static void CreateAndDisplayCar(ref IVehicleFactory factory)
+        {
+            factory = AutoMBuilder.Build(BodyType.Car);
+
+            IVehicle vehicle = factory.GetVehicle(IgnitionType.Automatic.ToString());
+            WriteLine($" Vehicle {factory.ToString()}");
+
+            vehicle = factory.GetVehicle(IgnitionType.Hybrid.ToString());
+            WriteLine($" Vehicle {factory.ToString()}");
+
+            vehicle = factory.GetVehicle(IgnitionType.None.ToString());
+            WriteLine($" Vehicle {factory.ToString()}");
+
+            ReadKey();
+        }
+
+        private static void CreateAndDisplayTruck(ref IVehicleFactory factory)
+        {
+            factory = AutoMBuilder.Build(BodyType.Truck);
+
+            IVehicle vehicle = factory.GetVehicle(IgnitionType.Automatic.ToString());
+            WriteLine($" Vehicle {vehicle.ToString()}");
+
+            vehicle = factory.GetVehicle(IgnitionType.Manual.ToString());
+            WriteLine($" Vehicle {vehicle.ToString()}");
+
+            vehicle = factory.GetVehicle(IgnitionType.None.ToString());
+            WriteLine($" Vehicle {vehicle.ToString()}");
+
+            ReadKey();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+Execrcise to learn Design Patterns.
+Naming convention : 
+	Folder Pre-fix 
+		1. Creational <Pattern Name>
+		2. Structural <Pattern Name>
+		3. Behavioral <Pattern Name>
+
+1. Singleton
+1. Abstract Factory
+
+2. Adapter - Not Started.
+
+3. Command - Not Started.
